ae6e2b11d19f6f77e420e117692bdb8a51e5a26b
Shehzad Shaikh <shaikhshehzad024@gmail.com>
2026-08-25 14:27:59 +0530

    Update: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.cpp
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> index;
-        int s = nums.size();
-        for(int i = 0; i < s; i++) {
-            // [3, 4, 5, 6], target 7
-            // diff = 4, not in map, 
-            // index[3] = 0
-            // diff = 3, index.find(3) = true
-            // 
+        map<int,int> index;
+        int size = nums.size();
+        for(int i = 0; i < size; i++) {
             int diff = target - nums[i];
             if (index.find(diff) != index.end()) {
                 return {index[diff], i};
@@ -17,4 +12,5 @@ public:
         }
         return {};
     }
+    
 };
