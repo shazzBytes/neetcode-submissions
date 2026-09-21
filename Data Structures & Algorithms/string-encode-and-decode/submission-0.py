1eced253549b72702c79e1ed0658a45b01897fcd
Shehzad Shaikh <shaikhshehzad024@gmail.com>
2026-09-22 04:47:21 +0530

    Update: string-encode-and-decode - submission-0

--- Data Structures & Algorithms/string-encode-and-decode/submission-0.py
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -1,36 +1,27 @@
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
-        eString = ""
+        eString = []
         # i am creating a header which would store the lengths of the string
         # then the header would end with $.
         # after that the strings would be just laid like that.
+        lens = [str(len(n)) for n in strs]
         for n in strs:
-            eString+= f"{len(n)}#"
-        eString+= "$"
+            eString.append(f"{len(n)}#")
+        eString.append("$")
         # header created. Now I can add the elements of the string
         for n in strs:
-            eString += n
-        return eString
+            eString.append(n)
+        return "#".join(lens) + "$" + "".join(strs)
             
             
 
     def decode(self, s: str) -> List[str]:
+        header, data = s.split("$", 1)
+        lens = [] if not header else map(int, header.split("#"))
         res = []
-        right = s.find("$")
-        lbuf = ""
-        lens  = 0
-        nStart = right+1
-        for n in range(right):
-            if s[n] != "#":
-                lbuf+= s[n]
-            else:
-                lens = int(lbuf)
-                lbuf = ""
-                dString = ""
-                for n in range(nStart, lens+nStart):
-                    dString+=s[n]
-                nStart += lens
-                res.append(dString)
+        pos = 0
+        for n in lens:
+            res.append(data[pos:pos+n])
+            pos+=n
         return res
-        
